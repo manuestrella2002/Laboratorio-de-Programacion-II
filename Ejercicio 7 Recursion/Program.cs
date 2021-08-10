e6192ce101a8a91ea2bf62abd9b4da9449bec822
Juan Estrella <manuestrella2002@gmail.com>
2021-08-10 18:36:39 -0300

    Ejercicio 7

---
+++ Ejercicio 7 Recursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Escribir una función recursiva que encuentra y 
+ * devuelve la suma de los elementos en
+ * un array entero.
+ */
+
+
+namespace Ejercicio_7_Recursion
+{
+    class Program
+    {
+        static int Suma(int[] num_, int n)
+        {
+            if (n==0)
+            {
+                return num_[0];
+            }
+            else
+            {
+                return Suma(num_, n - 1) + num_[n-1];
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] {1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine("La suma es: {0}", Suma(vector, 10)-1);
+
+
+        }
+    }
+}
