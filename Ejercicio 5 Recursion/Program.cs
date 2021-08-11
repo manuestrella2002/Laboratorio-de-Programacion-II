d5eeffec24bb8dff6017fc7be3027bf92acf351a
Juan Estrella <manuestrella2002@gmail.com>
2021-08-11 12:44:54 -0300

    Ejercicios varios

---
+++ Ejercicio 5 Recursion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5_Recursion
+{
+    class Program
+    {
+
+        static int Vocales(string Palabra)
+        {
+            int acum;
+            if (Palabra.Length==0)
+            {
+                return 0;
+
+            }
+            if(Palabra[Palabra.Length-1]=='a'|| Palabra[Palabra.Length - 1] == 'e'|| Palabra[Palabra.Length - 1] == 'i'|| Palabra[Palabra.Length - 1] == 'o'|| Palabra[Palabra.Length - 1] == 'u')
+            {
+                acum++;
+                return Vocales(Palabra.Substring(0, Palabra.Length - 1) + acum);
+            }
+            else
+            {
+                return Vocales(Palabra.Substring(0, Palabra.Length - 1));
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            string texto = "Hola" ;
+            Console.WriteLine("La cantidad de vocales es: {0}", Vocales(texto));
+
+        }
+    }
+}
---
+++ Ejercicio 6 Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6_Recursion
+{ 
+    class Program
+    {
+        static int SumaCuadrados(int N)
+        {
+            if (N < 0)
+            {
+                N = -N;
+            }
+
+            if (N == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return SumaCuadrados(N - 1) + (int)Math.Pow(N, 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int numero = -2;
+            Console.WriteLine("La suma de los cuadrados es: {0}", SumaCuadrados(numero));
+
+
+
+        }
+    }
+}
